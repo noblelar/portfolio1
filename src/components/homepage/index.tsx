d28c1373d1d66d8fd5e3a8e471d0a5df8be5672c
noblelar <59696875+noblelar@users.noreply.github.com>
2024-03-17 23:06:01 +0000

    stage3

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -29,9 +29,8 @@ const Homepage = () => {
       {/* Picture and Motivational quote */}
       <div className=" container m-auto ">
         <div className=" grid grid-cols-2 max-md:grid-cols-1 ">
-          <div>
+          <div className=" flex items-center justify-center ">
             <Image
-              // className="pt-2"
               className=""
               src={"/images/my_image_hd.png"}
               alt="My image"
@@ -40,13 +39,18 @@ const Homepage = () => {
               quality={100}
             />
           </div>
-          <div>
-            <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-              <div className="p-6">
-                <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                  The most certain way to succeed is to always try one more
-                  time.
+          <div className=" flex justify-center items-center m-auto max-lg:my-8 max-lg:mx-4 w-full ">
+            <div className="relative flex mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl justify-center items-center lg:w-[90%] " >
+              <div className="p-6 m-auto ">
+                <h5 className="block mb-2 font-playfair  font-semibold leading-snug tracking-normal text-blue-gray-900 lg:text-[3rem] text-justify italic text-[1.4rem] ">
+                  The most certain way to succeed is to always <br />
+                  <p className=" text-center w-full font-bold py-4 ">
+                    &#34; try one more time &#34;
+                  </p>
                 </h5>
+                <p className=" text-right p-4 pt-0 italic text-[1.2rem] max-lg:text-[1rem] ">
+                  ~ Thomas Edison
+                </p>
               </div>
             </div>
           </div>
