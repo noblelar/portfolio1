ba09940b6d6c7f47934de731e294c990ccc05d54
noblelar <59696875+noblelar@users.noreply.github.com>
2024-03-23 23:36:24 +0000

    stage3

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import type { ReactElement } from "react";
 import Header from './header';
 import Footer from './footer';
 
+
 export default function Layout({ children }: { children: ReactElement }) {
    return (
      <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
-
 import Homepage from "@/components/homepage";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <Homepage/>
-    </main>
+    <>
+      <Head>
+        <title>Nobleman</title>
+        <meta name="Nobleman" content="Nobleman's official Porfolio Website " />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/AN.png" />
+      </Head>
+      <main>
+        <Homepage />
+      </main>
+    </>
   );
 }
-
-
